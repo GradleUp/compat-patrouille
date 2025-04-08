79bc55e2422f62ee4a47cb1571acf3f199fca91e
Martin Bonnin <martin@mbonnin.net>
2025-04-08 18:31:35 +0200

    bootstrap ourselves

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import com.gradleup.librarian.gradle.Librarian
 plugins {
   alias(libs.plugins.kgp).apply(false)
   alias(libs.plugins.librarian).apply(false)
+  alias(libs.plugins.compat.patrouille).apply(false)
 }
 
 Librarian.root(project)
--- compat-patrouille-gradle-plugin/build.gradle.kts
+++ compat-patrouille-gradle-plugin/build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.api.internal.artifacts.dependencies.DefaultFileCollectionDepen
 plugins {
   id("org.jetbrains.kotlin.jvm")
   id("java-gradle-plugin")
+  id("com.gradleup.compat.patrouille")
 }
 
 Librarian.module(project)
@@ -16,6 +17,11 @@ dependencies {
 
 configurations.getByName("api").dependencies.removeIf {
   it is DefaultFileCollectionDependency
+}
+
+compatPatrouille {
+  java(11)
+  kotlin("2.0.21")
 }
 
 gradlePlugin {
