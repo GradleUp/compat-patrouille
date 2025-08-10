7103c6f61b9a3ab3431e0f30a88e6f5b8ba92cf5
Martin Bonnin <martin@mbonnin.net>
2025-08-10 13:00:25 +0200

    Bump bootstrap versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,10 @@ import com.gradleup.librarian.gradle.Librarian
 plugins {
   alias(libs.plugins.kgp).apply(false)
   alias(libs.plugins.librarian).apply(false)
+  alias(libs.plugins.nmcp).apply(false)
   alias(libs.plugins.compat.patrouille).apply(false)
   alias(libs.plugins.gratatouille).apply(false)
   alias(libs.plugins.ksp).apply(false)
 }
 
 Librarian.root(project)
-
-
