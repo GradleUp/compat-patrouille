ae9f111b52bf9b08e5294af6d3141299ed0a7d66
Martin Bonnin <martin@mbonnin.net>
2025-11-23 19:56:53 +0100

    Add a placeholder plugin

---
+++ compat-patrouille-gradle-plugin/src/main/kotlin/compat/patrouille/CompatPatrouillePlugin.kt
@@ -0,0 +1,9 @@
+package compat.patrouille
+
+import gratatouille.wiring.GPlugin
+import org.gradle.api.Project
+
+@GPlugin(id = "com.gradleup.compat.patrouille")
+fun compatPatrouillePlugin(target: Project) {
+  error("com.gradleup.compat.patrouille has been renamed to com.gradleup.topmac. See https://github.com/GradleUp/compat-patrouille/issues/5 for more details.")
+}
