74dd2eb30f67b8a857de359a031fbc6a3f605c60
Martin Bonnin <martin@mbonnin.net>
2025-11-26 18:38:58 +0100

    unbreak publishing snapshots

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
       maven("https://storage.googleapis.com/gradleup/m2") {
         content {
           includeModule("com.gradleup.gratatouille", "gratatouille-processor")
+          includeModule("com.gradleup.nmcp", "nmcp-tasks")
         }
       }
     }
