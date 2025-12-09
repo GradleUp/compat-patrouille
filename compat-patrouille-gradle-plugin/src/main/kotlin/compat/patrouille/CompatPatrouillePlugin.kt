6d1c059508176752961318dadb2e3fb6ca2fce98
Martin Bonnin <martin@mbonnin.net>
2025-12-09 16:32:51 +0100

    Remove compat-patrouille plugin (#64)

--- compat-patrouille-gradle-plugin/src/main/kotlin/compat/patrouille/CompatPatrouillePlugin.kt
+++
@@ -1,9 +0,0 @@
-package compat.patrouille
-
-import gratatouille.wiring.GPlugin
-import org.gradle.api.Project
-
-@GPlugin(id = "com.gradleup.compat.patrouille")
-fun compatPatrouillePlugin(target: Project) {
-  error("com.gradleup.compat.patrouille has been renamed to com.gradleup.tapmoc. See https://github.com/GradleUp/compat-patrouille/issues/5 for more details.")
-}
